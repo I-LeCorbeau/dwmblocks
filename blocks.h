8d0b90e4cf7c316a24dcfd4715d8f68fceaab55d
I-LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2020-04-14 14:53:55 -0400

    First commit

---
+++ blocks.h
@@ -0,0 +1,18 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/.build/dwmblocks/modules/chkupd",		120,		0},
+
+	{"", "~/.build/dwmblocks/modules/mem",			2,		9},
+	
+	{"", "~/.build/dwmblocks/modules/cpu",			1,		0},
+
+	{"", "~/.build/dwmblocks/modules/network",		5,		8},
+
+	{"", "~/.build/dwmblocks/modules/volume",		0,		10},
+	
+	{"", "~/.build/dwmblocks/modules/clock",		1,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
